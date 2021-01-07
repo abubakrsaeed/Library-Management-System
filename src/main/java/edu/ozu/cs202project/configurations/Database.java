4e66f842716c6af34d38108eb13e9e6a6a6e297a
Muhammad Abubakr Saeed <abubakr.saeed@ozu.edu.tr>
2021-01-08 01:33:15 +0500

    initial commit

---
+++ src/main/java/edu/ozu/cs202project/configurations/Database.java
@@ -0,0 +1,25 @@
+package edu.ozu.cs202project.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class Database {
+
+    @Bean
+    public DataSource MySQLDataSource() {
+        DriverManagerDataSource source = new DriverManagerDataSource();
+
+        source.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        source.setUrl("jdbc:mysql://localhost:3306/lms");
+        source.setUsername("root");
+        source.setPassword("12345");
+
+        return source;
+
+    }
+
+}
---
+++ src/main/java/edu/ozu/cs202project/services/LoginService.java
@@ -0,0 +1,24 @@
+package edu.ozu.cs202project.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class LoginService
+{
+    @Autowired
+    JdbcTemplate connection;
+
+    public boolean validate(String username, String password)
+    {
+        List<Map<String, Object>> response = connection.queryForList(
+                "SELECT * FROM users WHERE user_username = ? AND user_pass = ?", new Object[]{ username, password }
+        );
+
+        return response.size() == 1;
+    }
+}
