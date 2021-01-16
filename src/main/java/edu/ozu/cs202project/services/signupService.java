30b7c1d613b3b07c88ebd2cfe8a48a8a82095203
Klondayk <77169470+Klondayk@users.noreply.github.com>
2021-01-16 18:01:13 +0300

    Update signupService.java

--- src/main/java/edu/ozu/cs202project/services/signupService.java
+++ src/main/java/edu/ozu/cs202project/services/signupService.java
@@ -7,16 +7,29 @@ import org.springframework.stereotype.Service;
 @Service
 public class signupService {
 
-    @Autowired
+      @Autowired
     JdbcTemplate connection;
-
     public boolean usersignup(String user_name, String user_username, String user_pass, String user_phone)
     {
         int i = connection.update(
                 "INSERT INTO users (user_name, user_username, user_pass, user_phone) values (?, ?, ?, ?)",
                 new Object[]{user_name, user_username, user_pass, user_phone}
-        );
 
-        return i == 1;
+        );
+        int j = connection.update(
+                "INSERT INTO borrower_details (user_username,book_id,borrowed_on,due_date,return_date,days_overdue,penalty_charged) values (?,1,NULL,NULL,NULL,NULL,'0$')",
+                new Object[]{user_username});
+
+        return i == 1 && j==1;
+
+    }
+    public boolean usersignup2(String user_username)
+    {
+        int j = connection.update(
+                "INSERT INTO borrower_details (user_username,book_id,borrowed_on,due_date,return_date,days_overdue,penalty_charged) values (?,1,NULL,NULL,NULL,NULL,'0$')",
+        new Object[]{user_username});
+
+        return j == 1;
+
     }
 }
